e24aee4847c2a22f310fa626c639fccc211285ab
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-05-11 15:59:16 +0200

    add getfarmlands

---
+++ getfarmlands/index.php
@@ -0,0 +1,26 @@
+<?Php
+ include("../lib/conn.php");
+
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    $valid = false;
+  }
+
+  $sql = "SELECT * FROM `farmlands`";
+  $result = $conn->query($sql);
+  $rows = array();
+  if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+      $rows[] = $row;
+    }
+  } else {
+  
+  }
+
+print_r(json_encode($rows));
+
+?>
---
+++ lib/conn.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "qw3077p0_ema";
+$password = "Mmy^6g010";
+$dbname = "qw3077p0_ema";
+?>
